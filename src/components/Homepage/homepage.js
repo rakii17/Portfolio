c65f7234e391b4dc9f31e51b4ff10ef897da7989
Rakesh <rakesh.717.02@gmail.com>
2024-06-18 12:57:50 +0530

    Edited Homepage component

--- src/components/Homepage/homepage.js
+++ src/components/Homepage/homepage.js
@@ -9,12 +9,11 @@ const Homepage = () => {
     <section id="intro">
       <div className="introContent">
       <img src={profilePic} alt="Profile" className="backgroundIntroImg" />
-        <div className="hello">Hello,</div>
-        <span className="introText">
-          I'm <span className="introName">Rakesh</span> <br /> Website Designer <br/> Data Analyst
+        <div className="introText">Hello, I'm <span className="introName">Rakesh</span></div>
+        <span className="introSubText"> Data Analyst | Website Designer
         </span>
         <p className="introPara">
-        As a recent Bachelor of Engineering graduate from BMS Institute of Technology and Management, I specialize in data analysis and software development using tools like Excel, Python, Power BI, Tableau, MySQL, and MongoDB. I am passionate about driving strategic decisions and business growth. My interests in graphic design, reading, writing fictions and playing cricket. Let's connect to explore how my analytical and technical expertise can add value to your team.
+        As a recent B.E. graduate from BMS Institute of Technology and Management, I specialize in data analysis and software development using Excel, Python, Power BI, Tableau, MySQL, and MongoDB. I am passionate about driving strategic decisions and business growth. Let's connect to explore how my analytical and technical expertise can add value to your team.
         </p>
         <Link to="/contact">
           <button className="btn">
