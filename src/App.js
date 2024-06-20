f99c761d55061b7a140ae32f138bcd26313f03cb
Rakesh <rakesh.717.02@gmail.com>
2024-06-20 12:22:17 +0530

    Added Skills section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/navbar';
 import Homepage from './components/Homepage/homepage';
 import Skills from './components/Skills/skills';
@@ -7,6 +6,7 @@ import Projects from './components/Projects/projects';
 import Works from './components/Works/works';
 import Studies from './components/Studies/studies';
 import Footer from './components/Footer/footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
         <Homepage />
         <Skills />
         <Projects />
+        <Works />
         <Studies />
         <Footer />
       </div>
--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import './skills.css';
+import React from 'react';
+import reactImg from '../../assets/reactLogo.png';
+import UiImg from '../../assets/Ui.jpg';
+import FigmaImg from '../../assets/Figma.jpg';
 
 const Skills = () => {
   return (
-    <div>
-      i ma skill
-    </div>
+    <section id="skills">
+      <div className="skillContent">
+        <div className="skillTitle">Skills</div>
+        <span className="skillDesc">
+        I have a strong foundation in data analysis and front-end development, complemented by expertise in UI/UX design. This combination of skills enables me to create visually appealing and user-friendly interfaces while effectively analyzing and interpreting data.</span>
+        <div className="skillBars">
+          <div className="skillBar">
+            <img src={UiImg} alt="Ui" className="skillBarImg" />
+            <div className="skillBarText">
+              <h2>Data <br/>Analysis</h2>
+              <p>Click here to view works </p>
+            </div>
+          </div>
+
+          <div className="skillBar">
+            <img src={FigmaImg} alt="Figma" className="skillBarImg" />
+            <div className="skillBarText">
+              <h2>Front-end Development</h2>
+              <p>Click here to view works </p>
+            </div>
+          </div>
+
+          <div className="skillBar">
+            <img src={FigmaImg} alt="Figma" className="skillBarImg" />
+            <div className="skillBarText">
+              <h2>UI / UX <br/>Design</h2>
+              <p>Click here to view works </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
