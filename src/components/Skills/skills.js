7a3248cae9f812884b0326709e3d635a2b2f6dcb
Rakesh <rakesh.717.02@gmail.com>
2024-06-20 13:59:49 +0530

    Added responsiveness to skill section

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -10,12 +10,12 @@ const Skills = () => {
       <div className="skillContent">
         <div className="skillTitle">Skills</div>
         <span className="skillDesc">
-        I have a strong foundation in data analysis and front-end development, complemented by expertise in UI/UX design. This combination of skills enables me to create visually appealing and user-friendly interfaces while effectively analyzing and interpreting data.</span>
+        I have a solid background in data analysis and front-end development, complemented by strong expertise in UI/UX design. This allows me to create visually appealing interfaces that are user-friendly, <br/>while effectively analyzing and interpreting data.</span>
         <div className="skillBars">
           <div className="skillBar">
             <img src={UiImg} alt="Ui" className="skillBarImg" />
             <div className="skillBarText">
-              <h2>Data <br/>Analysis</h2>
+              <h2>Data Analysis</h2>
               <p>Click here to view works </p>
             </div>
           </div>
@@ -31,7 +31,7 @@ const Skills = () => {
           <div className="skillBar">
             <img src={FigmaImg} alt="Figma" className="skillBarImg" />
             <div className="skillBarText">
-              <h2>UI / UX <br/>Design</h2>
+              <h2>UI / UX  Design</h2>
               <p>Click here to view works </p>
             </div>
           </div>
