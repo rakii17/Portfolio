82b98290f95b98458b2e5f430b522b63f4637492
Rakesh <rakesh.717.02@gmail.com>
2024-06-21 21:42:57 +0530

    Updating Navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -6,12 +6,12 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navMenuItems">
-        <Link to="homepage" spy={true} smooth={true} offset={-30} duration={100} className="navMenuItem">Home</Link>
-        <Link to="skills" spy={true} smooth={true} offset={10} duration={600} className="navMenuItem">Skills</Link>
-        <Link to="projects" spy={true} smooth={true} offset={-15} duration={700} className="navMenuItem">Projects</Link>
-        <Link to="works" spy={true} smooth={true} offset={-30} duration={1200} className="navMenuItem">Works</Link>
-        <Link to="studies" spy={true} smooth={true} offset={-30} duration={1200} className="navMenuItem">Studies</Link>
-        <Link to="footer" spy={true} smooth={true} offset={-30} duration={1200} className="navMenuItem">Contact</Link>
+        <Link to="homepage" spy={true} smooth={true} offset={-100} duration={400} className="navMenuItem">Home</Link>
+        <Link to="skills" spy={true} smooth={true} offset={0} duration={600} className="navMenuItem">Skills</Link>
+        <Link to="projects" spy={true} smooth={true} offset={-100} duration={700} className="navMenuItem">Projects</Link>
+        <Link to="works" spy={true} smooth={true} offset={-150} duration={1200} className="navMenuItem">Works</Link>
+        <Link to="studies" spy={true} smooth={true} offset={-65} duration={1200} className="navMenuItem">Studies</Link>
+        <Link to="footer" spy={true} smooth={true} offset={-30} duration={800} className="navMenuItem">Contact</Link>
       </div>
     </nav>
   );
