41a59f1098819d0e74facc68c5448fc8e088e350
Rakesh <rakesh.717.02@gmail.com>
2024-06-22 06:52:41 +0530

    Changed Homepage Description

--- src/components/Homepage/homepage.js
+++ src/components/Homepage/homepage.js
@@ -13,7 +13,7 @@ const Homepage = () => {
         <span className="introSubText"> Data Analyst | Website Designer
         </span>
         <p className="introPara">
-        As a recent B.E. graduate from BMS Institute of Technology and Management, I specialize in data analysis and software development using Excel, Python, Power BI, Tableau, MySQL, and MongoDB. I am passionate about driving strategic decisions and business growth. Let's connect to explore how my analytical and technical expertise can add value to your team.
+        I'm a recent Engineering graduate from BMS Institute of Technology and Management, I specialize in data analysis and tools like Excel, Python, Power BI, Tableau and MySQL. I am skilled in ReactJS, JavaScript and Media query as am interested in Frontend Development. My hobbies are graphic designing, reading, writing fictions and playing cricket. Let's connect to explore how my analytical and technical expertise can add value to your team.
         </p>
         <Link to="/contact">
           <button className="btn">
--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,6 +1,5 @@
 import './skills.css';
 import React from 'react';
-import reactImg from '../../assets/reactLogo.png';
 import UiImg from '../../assets/Ui.jpg';
 import FigmaImg from '../../assets/Figma.jpg';
 
@@ -10,13 +9,15 @@ const Skills = () => {
       <div className="skillContent">
         <div className="skillTitle">Skills</div>
         <span className="skillDesc">
-        I have a solid background in data analysis and front-end development, complemented by strong expertise in UI/UX design. This allows me to create visually appealing interfaces that are user-friendly, <br/>while effectively analyzing and interpreting data.</span>
+          I have a solid background in data analysis and front-end development, complemented by strong expertise in UI/UX design. This allows me to create visually appealing interfaces that are user-friendly,while effectively analyzing and interpreting data.</span>
         <div className="skillBars">
           <div className="skillBar">
             <img src={UiImg} alt="Ui" className="skillBarImg" />
             <div className="skillBarText">
               <h2>Data Analysis</h2>
-              <p>Click here to view works </p>
+              <p><a href="https://github.com/rakii17" target="_blank" rel="noopener noreferrer">
+                 Click here to view works
+                </a></p>
             </div>
           </div>
 
@@ -24,7 +25,9 @@ const Skills = () => {
             <img src={FigmaImg} alt="Figma" className="skillBarImg" />
             <div className="skillBarText">
               <h2>Front-end Dev</h2>
-              <p>Click here to view works </p>
+              <p><a href="https://github.com/rakii17" target="_blank" rel="noopener noreferrer">
+                 Click here to view works
+                </a></p>
             </div>
           </div>
 
@@ -32,7 +35,11 @@ const Skills = () => {
             <img src={FigmaImg} alt="Figma" className="skillBarImg" />
             <div className="skillBarText">
               <h2>UI / UX  Design</h2>
-              <p>Click here to view works </p>
+              <p>
+                <a href="https://github.com/rakii17" target="_blank" rel="noopener noreferrer">
+                 Click here to view works
+                </a>
+              </p>
             </div>
           </div>
         </div>
